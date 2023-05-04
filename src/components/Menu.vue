660b5ff4014aa62531ff34d3c7f469a79b5a4b94
Alejandro Angel <dazaange247@gmail.com>
2023-05-03 21:40:34 -0500

    comentario

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,36 +1,464 @@
 <template>
   <div class="greetings">
-    <h1 class="green">sdfsdfsdf</h1>
-    <h3>
-      You’ve successfully created a project with
-    </h3>
+    <header>
+        <nav>
+            <div class="links">
+                <a class="nav-logo" href="#">
+                    <img src="images/Logo.svg" alt="">
+                </a>
+                <div class="cont-links">
+                    <a href="#" >FOREX</a>
+                    <a href="#" >STOCKS</a>
+                    <a href="#" >FUTURES</a>
+                    <a href="#" >TradeGATEHub</a>
+                </div>
+            </div>
+            <div class="login">
+                <ul class="nav-ul">
+                    <li class="nav-li">
+                        <a href="#" data-toggle="sub-menu" class="nav-link">Log in
+                            <img src="images/arrow.svg" alt="">
+                        </a>
+                        <ul class="sub-ul">
+                            <li class="nav-li">
+                                <a href="#" class="nav-link">Project 1</a>
+                            </li>
+                            <li class="nav-li">
+                                <a href="#" class="nav-link">Project 2</a>
+                            </li>
+                            <li class="nav-li">
+                                <a href="#" class="nav-link">Project 3</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-li">
+                        <a href="#" class="nav-link">home</a>
+                    </li>
+                    <li class="nav-li">
+                        <a href="#" data-toggle="sub-menu" class="nav-link">Pricing
+                            <img src="images/world-icon.svg" alt="">
+                            <img src="images/arrow.svg" alt="">
+                        </a>
+                        <ul class="sub-ul">
+                            <li class="nav-li">
+                            <a href="#" class="nav-link">Project 1</a>
+                            </li>
+                            <li class="nav-li">
+                            <a href="#" class="nav-link">Project 2</a>
+                            </li>
+                            <li class="nav-li">
+                            <a href="#" class="nav-link">Project 3</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <nav class="nav-container">
+            <div class="hamberger-btn">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24" height="24" viewBox="0 0 24 24">
+                    <path fill="#FFFFFF" d="M3 18h13v-2H3v2zm0-5h10v-2H3v2zm0-7v2h13V6H3zm18 9.59L17.42 12L21 8.41L19.59 7l-5 5l5 5L21 15.59z"></path>
+                  </svg>
+            </div>
+            <ul class="nav-ul">
+                <li class="nav-li">
+                    <a href="#" class="nav-link ">Markets</a>
+                </li>
+                <li class="nav-li">
+                    <a href="#" data-toggle="sub-menu" class="nav-link ">Company
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24" height="24" viewBox="0 0 24 24">
+                        <path fill="#FFFFFF" d="M8.12 9.29L12 13.17l3.88-3.88a.996.996 0 1 1 1.41 1.41l-4.59 4.59a.996.996 0 0 1-1.41 0L6.7 10.7a.996.996 0 0 1 0-1.41c.39-.38 1.03-.39 1.42 0z"></path>
+                        </svg>
+                    </a>
+                    <ul class="sub-ul">
+                        <li class="nav-li">
+                            <a href="#" class="nav-link ">Company 1</a>
+                        </li>
+                        <li class="nav-li">
+                            <a href="#" class="nav-link ">Company 2</a>
+                        </li>
+                        <li class="nav-li">
+                            <a href="#" class="nav-link ">Company 3</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-li">
+                    <a href="#" class="nav-link ">Accounts</a>
+                </li>
+                <li class="nav-li">
+                    <a href="#" data-toggle="sub-menu" class="nav-link ">Platforms
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24" height="24" viewBox="0 0 24 24">
+                        <path fill="#FFFFFF" d="M8.12 9.29L12 13.17l3.88-3.88a.996.996 0 1 1 1.41 1.41l-4.59 4.59a.996.996 0 0 1-1.41 0L6.7 10.7a.996.996 0 0 1 0-1.41c.39-.38 1.03-.39 1.42 0z"></path>
+                        </svg>
+                    </a>
+                    <ul class="sub-ul">
+                        <li class="nav-li">
+                        <a href="#" class="nav-link ">Plataforms 1</a>
+                        </li>
+                        <li class="nav-li">
+                        <a href="#" class="nav-link ">Plataforms 2</a>
+                        </li>
+                        <li class="nav-li">
+                        <a href="#" class="nav-link ">Plataforms 3</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-li">
+                    <a href="#" class="nav-link ">Pricing</a>
+                </li>
+                <li class="nav-li">
+                    <a href="#" class="nav-link ">Introducing Brokers</a>
+                </li>
+                <li class="nav-li">
+                    <a href="#" class="nav-link ">Education</a>
+                </li>
+                <li class="nav-li">
+                    <a href="#" data-toggle="sub-menu" class="nav-link">Support
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24" height="24" viewBox="0 0 24 24">
+                        <path fill="#FFFFFF" d="M8.12 9.29L12 13.17l3.88-3.88a.996.996 0 1 1 1.41 1.41l-4.59 4.59a.996.996 0 0 1-1.41 0L6.7 10.7a.996.996 0 0 1 0-1.41c.39-.38 1.03-.39 1.42 0z"></path>
+                        </svg>
+                    </a>
+                    <ul class="sub-ul">
+                        <li class="nav-li">
+                        <a href="#" class="nav-link">Support 1</a>
+                        </li>
+                        <li class="nav-li">
+                        <a href="#" class="nav-link">Support 2</a>
+                        </li>
+                        <li class="nav-li">
+                        <a href="#" class="nav-link">Support 3</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </header>
   </div>
 </template>
 
 <script setup>
-
+(() => {
+    const navUl = document.querySelector(".nav-ul"),
+      hambergerBtn = document.querySelector(".hamberger-btn");
+    hambergerBtn.addEventListener("click", toggleMenu);
+    navUl.addEventListener("click", toggleSubMenu);
+    function toggleMenu() {
+      hambergerBtn.children[0].classList.toggle("rotate");
+      navUl.classList.toggle("show");
+    }
+  
+    function toggleSubMenu(e) {
+      if (e.target.hasAttribute("data-toggle")) {
+        e.target.nextElementSibling.classList.toggle("active");
+      }
+    }
+  })();
 </script>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
+header {
+    background: linear-gradient(rgb(24, 31, 41) 0%, rgb(24, 31, 41) 46%, rgba(24, 31, 41, 0) 100%);
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    padding: 0px 52px 57px;
+    flex-direction: column;
+    gap: 25px;
+    z-index: 1;
+    position: relative;
 }
 
-h3 {
-  font-size: 1.2rem;
+nav:nth-child(1) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 }
 
-.greetings h1,
-.greetings h3 {
-  text-align: center;
+.links {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 50%;
 }
 
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+.cont-links {
+    display: flex;
+    gap: 25px;
+    font-size: 16px;
+    letter-spacing: 2px;
+}
+
+.cont-links a {
+    color: white;
+    font-weight: 900;
+}
+
+.cont-links a.slide-horizontal .char::before {
+    color: red;
+}
+
+.login {
+    display: flex;
+    padding-right: 45px;
+}
+
+.login .nav-ul {
+    gap: 0px;
+    position: relative;
+}
+
+.login .nav-ul a {
+    font-weight: 300;
+    font-size: 16px;
+    position: relative;
+}
+
+.login .nav-ul li:nth-child(1) a {
+    font-weight: 400;
+}
+
+.login .nav-ul li:first-child a::before {
+    content: "";
+    width: 1px;
+    height: 21px;
+    background: white;
+    position: absolute;
+    right: 0px;
+    opacity: 0.5;
+}
+
+.login .nav-li:hover > .sub-ul {
+    transform: translateY(0px);
+}
+
+.login .nav-ul li:last-child a img:first-child {
+    width: 16px;
+}
+
+.nav-container {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.nav-logo {
+    display: inline-flex;
+    align-items: center;
+    height: 80px;
+}
+
+.nav-logo svg {
+    width: 60px;
+    height: 60px;
+    padding: 10px;
+}
+
+.logo-text {
+    color: rgb(255, 255, 255);
+    font-size: 2rem;
+    font-weight: 600;
+}
+
+.hamberger-btn {
+    cursor: pointer;
+    background-color: rgb(52, 70, 93);
+    border-radius: 5px;
+    width: 40px;
+    height: 40px;
+    margin: 10px;
+    text-align: center;
+    vertical-align: middle;
+    transition: all 0.3s linear 0s;
+}
+
+.hamberger-btn svg {
+    transition: all 0.3s linear 0s;
+    margin: 2px 0px;
+    width: 90%;
+    height: 90%;
+}
+
+.hamberger-btn svg.rotate {
+    transform: rotateY(180deg);
+}
+
+.hamberger-btn:hover {
+    box-shadow: rgb(255, 255, 255) 0px 0px 10px;
+}
+
+.hamberger-btn:hover > svg {
+    animation: 0.3s ease 0s 1 normal forwards running hambergerBtnRotate;
+}
+
+.nav-ul {
+    display: flex;
+    align-items: center;
+    gap: 40px;
+}
+
+.nav-li {
+    height: 100%;
+}
+
+.nav-li:hover > .sub-ul {
+    opacity: 1;
+    visibility: visible;
+    transform: translateY(-58px);
+}
+
+.nav-li:hover > .nav-link {
+    color: white;
+}
+
+.nav-li:hover > .nav-link svg {
+    transform: rotate(180deg);
+}
+
+.nav-link {
+    padding: 15px;
+}
+
+.nav-link, .nav-link svg {
+    color: rgb(255, 255, 255);
+    font-weight: 300;
+    transition: all 0.3s ease-in-out 0s;
+    display: flex;
+    align-items: center;
+    gap: 2px;
+    font-size: 18px;
+}
+
+.nav-link:hover {
+    color: white;
+}
+
+.nav-link:hover > svg {
+    color: rgb(236, 195, 11);
+    transform: rotate(180deg);
+}
+
+.nav-link svg {
+    height: 19.2px;
+    vertical-align: middle;
+}
+
+.nav-link img {
+    width: 8px;
+    margin-left: 7px;
+}
+
+.sub-ul {
+    opacity: 0;
+    visibility: hidden;
+    transform: translateY(10px);
+    transition: all 0.3s linear 0s;
+    position: absolute;
+    top: 100%;
+    background-color: rgb(40, 50, 60);
+    width: 100%;
+    max-width: 200px;
+}
+
+.sub-ul .nav-li a {
+    display: block;
+    padding: 15px 30px;
+    transition: all 0.7s ease 0s;
+}
+
+.sub-ul .nav-li a::before {
+    display: none;
+}
+
+.sub-ul .nav-li a:hover {
+    background-color: rgb(52, 70, 93);
+}
+
+.splitting .word, .splitting .char {
+    display: inline-block;
+}
+
+.splitting .char {
+    position: relative;
+}
+
+.splitting .char::before, .splitting .char::after {
+    content: attr(data-char);
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    visibility: hidden;
+    transition: inherit;
+    user-select: none;
+}
+
+.splitting {
+    --word-center: calc((var(--word-total) - 1) / 2);
+    --char-center: calc((var(--char-total) - 1) / 2);
+    --line-center: calc((var(--line-total) - 1) / 2);
+}
+
+.splitting .word {
+    --word-percent: calc(var(--word-index) / var(--word-total));
+    --line-percent: calc(var(--line-index) / var(--line-total));
+}
+
+.splitting .char {
+    --char-percent: calc(var(--char-index) / var(--char-total));
+    --char-offset: calc(var(--char-index) - var(--char-center));
+    --distance: calc( (var(--char-offset) * var(--char-offset)) / var(--char-center) );
+    --distance-sine: calc(var(--char-offset) / var(--char-center));
+    --distance-percent: calc((var(--distance) / var(--char-center)));
+}
+
+.slide-vertical .char, .slide-horizontal .char {
+    overflow: hidden;
+    color: transparent;
+}
+
+.slide-vertical .char::before, .slide-horizontal .char::before, .slide-vertical .char::after, .slide-horizontal .char::after {
+    visibility: visible;
+    color: rgb(255, 255, 255);
+    transition-duration: 0.5s;
+    transition-timing-function: cubic-bezier(0.9, 0, 0.2, 1);
+    transition-property: transform;
+    transition-delay: calc(0.2s + ( 0.02s * ( var(--char-index)) ));
+}
+
+.slide-vertical .char::before, .slide-horizontal .char::before {
+    color: white;
+    transition-delay: calc(0.02s * ( var(--char-index)));
+}
+
+.slide-vertical:hover .char::before, .slide-horizontal:hover .char::before {
+    transition-delay: calc(0.2s + ( 0.02s * ( var(--char-index)) ));
+}
+
+.slide-vertical:hover .char::after, .slide-horizontal:hover .char::after {
+    transition-delay: calc(0.02s * ( var(--char-index)));
+}
+
+.slide-vertical .char::before {
+    transform: translateY(-100%);
+}
+
+.slide-vertical:hover .char::before {
+    transform: translateY(0%);
+}
+
+.slide-vertical:hover .char::after {
+    transform: translateY(100%);
+}
+
+.slide-horizontal .char::before {
+    transform: translateX(110%);
+}
+
+.slide-horizontal:hover .char::before {
+    transform: translateX(0%);
+}
+
+.slide-horizontal:hover .char::after {
+    transform: translateX(-110%);
 }
 </style>
